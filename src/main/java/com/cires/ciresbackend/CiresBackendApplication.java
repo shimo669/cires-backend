6ad675ca9ce4444557c0d8a3e157a18d608985f7
shimo669 <rutayisireshimoines@gmail.com>
2026-02-25 16:30:34 +0200

    ...

---
+++ src/main/java/com/cires/ciresbackend/CiresBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cires.ciresbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiresBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiresBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cires/ciresbackend/CiresBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cires.ciresbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiresBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
